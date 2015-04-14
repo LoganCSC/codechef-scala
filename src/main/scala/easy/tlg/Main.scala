f6726c6bc0946eab5d93ae715af72040f2e98277
Thuy Trinh <thuy@skedgo.com>
2015-04-14 23:42:02 +0700

    Submit 'The Lead Game'

---
+++ src/main/scala/easy/tlg/Main.scala
@@ -0,0 +1,33 @@
+package easy.tlg
+
+/**
+ * http://www.codechef.com/problems/TLG
+ */
+object Main {
+  def main(args: Array[String]) {
+    val testCount = readInt()
+    var lead1 = 0
+    var lead2 = 0
+    var prevScore1 = 0
+    var prevScore2 = 0
+    for (i <- 0 until testCount) {
+      val scores = readLine().split(" ")
+      val score1 = scores(0).toInt + prevScore1
+      prevScore1 = score1
+
+      val score2 = scores(1).toInt + prevScore2
+      prevScore2 = score2
+
+      val diff = score1 - score2
+      if (diff > 0 && diff > lead1)
+        lead1 = diff
+      else if (diff < 0 && -diff > lead2)
+        lead2 = -diff
+    }
+
+    if (lead1 > lead2)
+      println("1 %d".format(lead1))
+    else
+      println("2 %d".format(lead2))
+  }
+}
