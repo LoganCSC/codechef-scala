4b3cde31ab814e1710499db4fb9a30b4627fdb09
barrybecker4 <barrybecker4@yahoo.com>
2016-11-22 09:34:53 -0800

    Switch to state only implementation. The state is the result.

--- src/main/scala/experiments/candymachine/State.scala
+++ src/main/scala/experiments/candymachine/State.scala
@@ -3,22 +3,19 @@ package experiments.candymachine
 import State.unit
 
 
-case class State[S](run: S => (S)) {
+case class State[S](run: S => S) {
 
-  def map[A, B](f: S => S): State[S] = flatMap(a => unit(f(a)))
+  def map(f: S => S): State[S] = flatMap(s => unit(f(s)))
 
-  def flatMap(f: (S) => State[S]): State[S] = State(s => {
-    val s1 = run(s)
-    f(s).run(s1)
-  })
+  def flatMap(f: (S) => State[S]): State[S] = State(s => f(s).run(run(s)) )
 }
 
 
 object State {
 
-  def unit[S, A](a: A): State[S] = State(s => s)
+  def unit[S](a: S): State[S] = State(s => s)
 
-  def sequence[S, A](sas: List[State[S]]): State[S] = {
+  def sequence[S](sas: List[State[S]]): State[S] = {
     def go(s: S, actions: List[State[S]]): S =
       actions match {
         case Nil => s
@@ -28,9 +25,9 @@ object State {
   }
 
   def modify[S](f: S => S): State[S] = for {
-    s <- get       // Gets the current state and assigns it to `s`.
-    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
-  } yield s
+      s: S <- get       // Gets the current state and assigns it to `s`.
+      _ <- set(f(s))    // Sets the new state to `f` applied to `s`.
+    } yield s
 
   def get[S]: State[S] = State(s => s)
   def set[S](s: S): State[S] = State(_ => s)
