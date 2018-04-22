993978dd1501586d4ef0e8ab68833cee258fbec0
barrybecker4 <barrybecker4@yahoo.com>
2018-04-22 09:59:50 -0700

    Add 2018 dominating disney traveling salesman type problem. Example tests passing.

--- src/main/scala/codequest2018/p18dominatingdisney/DisneyPathFinder.scala
+++ src/main/scala/codequest2018/p18dominatingdisney/DisneyPathFinder.scala
@@ -26,8 +26,8 @@ class DisneyPathFinder(val weights: Seq[Int]) {
     var shortestPath: Seq[Path] = Seq()
 
     // never visit a destination more than twice since that would indicate a cycle.
-    val visitCount: Map[String, Int] = destinations.map(d => d -> 0).toMap
-    val allPaths = findAllPaths(Seq(), destinations, visitCount)
+    val visitCount: Map[String, Int] = Map()
+    val allPaths = findAllPaths(Seq(), visitCount)
 
     for (path <- allPaths) {
       val dist = path.map(_.weight).sum
@@ -39,16 +39,14 @@ class DisneyPathFinder(val weights: Seq[Int]) {
     shortestPath.map(_.num + 1).mkString(" ")
   }
 
-  private def findAllPaths(pathPrefix: Seq[Path],
-                           unvisited: Set[String],
-                           visitCount: Map[String, Int]): Seq[Seq[Path]] = {
+  private def findAllPaths(pathPrefix: Seq[Path], visitCount: Map[String, Int]): Seq[Seq[Path]] = {
 
     val currentPath = pathPrefix.lastOption
     val to = if (currentPath.isDefined) currentPath.get.to else "start"
 
-    if (unvisited.isEmpty) {
+    if (visitCount.keys.size == 4) {
       println("Visited everything! returning:  " + (pathPrefix :+ currentPath.get))
-      Seq(pathPrefix :+ currentPath.get)
+      Seq(pathPrefix)
     }
     else {
       var pathSeqs: Seq[Seq[Path]] = Seq()
@@ -62,11 +60,16 @@ class DisneyPathFinder(val weights: Seq[Int]) {
 
       for (childPath <- childPaths.sortBy(_.weight)) {
         val newPathPrefix = pathPrefix :+ childPath
-        val newUnvisited = if (destinations.contains(childPath.to)) unvisited - childPath.to else unvisited
+        val next = childPath.to
         val newVisitCount =
-          if (childPath.to != "start") { visitCount + (childPath.to -> (visitCount(childPath.to) + 1)) } else visitCount
-        if (newPathPrefix.map(_.weight).sum <= maxPathLen && (childPath.to == "start" || visitCount(childPath.to) <= 2))
-          pathSeqs ++= findAllPaths(newPathPrefix, newUnvisited, newVisitCount)
+          if (next != "start") {
+            if (visitCount.contains(next))
+              visitCount + (next -> (visitCount(next) + 1))
+            else visitCount + (next -> 1)
+          } else visitCount
+        val visitedThrice = next != "start" && visitCount.contains(next) && visitCount(next) > 2
+        if (newPathPrefix.map(_.weight).sum <= maxPathLen && !visitedThrice)
+          pathSeqs ++= findAllPaths(newPathPrefix, newVisitCount)
       }
       pathSeqs
     }
--- src/test/scala/codequest2018/p18dominatingdisney/SolutionSuite.scala
+++ src/test/scala/codequest2018/p18dominatingdisney/SolutionSuite.scala
@@ -7,8 +7,16 @@ import org.scalatest.FunSuite
   * @author Barry Becker
   */
 class SolutionSuite extends FunSuite {
-  test("test find shortest path") {
-    assertResult("1 2 4 6 6") { new DisneyPathFinder(Seq(5, 10, 7, 6, 2, 4, 12)).findShortestPath() }
+  test("test find shortest path case 1") {
+    assertResult("1 2 4 6") { new DisneyPathFinder(Seq(5, 10, 7, 6, 2, 4, 12)).findShortestPath() }
+  }
+
+  test("test find shortest path case 2") {
+    assertResult("7 6 4 2") { new DisneyPathFinder(Seq(15, 8, 20, 7, 20, 5, 10)).findShortestPath() }
+  }
+
+  test("test find shortest path case 3") {
+    assertResult("1 2 4 6") { new DisneyPathFinder(Seq(4, 8, 10, 6, 6, 8, 10)).findShortestPath() }
   }
 
 }
