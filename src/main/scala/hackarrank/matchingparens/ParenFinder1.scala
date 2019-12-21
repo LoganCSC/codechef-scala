c7bfbd0427cddfcfa35d825fabe16aef9ad41600
Barry Becker <barrybecker4@gmail.com>
2019-12-21 06:44:28 -0800

    Try to fix first version, but it does not work.

--- src/main/scala/hackarrank/matchingparens/ParenFinder1.scala
+++ src/main/scala/hackarrank/matchingparens/ParenFinder1.scala
@@ -1,6 +1,6 @@
 package hackarrank.matchingparens
 
-/** Note: This approach does not work because it will miss (())(()), for example, in the n= 4 case. */
+/** this approach does not work unfortunately */
 class ParenFinder1 extends ParenFinder {
 
   def findParenCombinations(n: Int): Seq[String] = {
@@ -11,8 +11,20 @@ class ParenFinder1 extends ParenFinder {
   private def findParens(n: Int): Set[String] = {
     if (n == 1) Set("()")
     else {
+      var combos: Set[String] = Set()
+
+      if (n % 2 == 0) {
+        val parens = findParens(n / 2)
+        combos ++= parens.flatMap(s => Set(s + s))
+      }
+      else if (n > 2) {
+        val parens = findParens((n - 1) / 2)
+        combos ++= parens.flatMap(s => Set(s + "()" + s))
+      }
+
       val parens = findParens(n - 1)
-      parens.flatMap(s => Set("()" + s, "(" + s + ")", s + "()"))
+      combos ++= parens.flatMap(s => Set("()" + s, "(" + s + ")", s + "()"))
+      combos
     }
   }
 }
--- src/test/scala/hackerrank/matchingparens/SolutionTest.scala
+++ src/test/scala/hackerrank/matchingparens/SolutionTest.scala
@@ -7,13 +7,13 @@ import testsupport.TestSupport.strip
 
 class SolutionTest extends FunSuite {
 
-  val pf = new ParenFinder1()
+  val pf = new ParenFinder2()
 
   test("test n = 2") {
     verifyCombos(2, Seq("(())", "()()") )
   }
 
-  test("test n = 3") {
+  test("test n = 3") { // 5 combos
     verifyCombos(3, Seq(
       "((()))",
       "(()())",
@@ -23,7 +23,7 @@ class SolutionTest extends FunSuite {
     ))
   }
 
-  test("test n = 4") {
+  test("test n = 4") {  // 14 combos
     verifyCombos(4, Seq(
       "(((())))",
       "((()()))",
@@ -42,7 +42,9 @@ class SolutionTest extends FunSuite {
     ))
   }
 
-  test("test n = 5") {
+
+
+  test("test n = 5") { // 42 combos  (initially got 37)
     verifyCombos(5, Seq(
       "((((()))))",
       "(((()())))",
@@ -89,7 +91,6 @@ class SolutionTest extends FunSuite {
     ))
   }
 
-
   test("test n = 6") {
     verifyNum(6, 132)
   }
@@ -105,18 +106,28 @@ class SolutionTest extends FunSuite {
   test("test n = 10") {
     verifyNum(10, 16796)
   }
+  test("test n = 11") {
+    verifyNum(11, 58786)
+  }
   test("test n = 12") {
     verifyNum(12, 208012)
   }
   test("test n = 14") {
+    val start = System.currentTimeMillis()
     verifyNum(14, 2674440)  // > 2 million combinations
+    println("took " + (System.currentTimeMillis() - start) + " milliseconds")
   }
+//  test("test n = 15") {
+//    val start = System.currentTimeMillis()
+//    verifyNum(15, 9694845)  // almost 10 million combinations
+//    println("took " + (System.currentTimeMillis() - start) + " milliseconds")
+//  }
 
   def verifyCombos(n: Int, expectedResult: Seq[String]): Unit = {
-    //val result1 = new ParenFinder1().findParenCombinations(n)
-    val result2 = new ParenFinder2().findParenCombinations(n)
-    //verifyResult(n, expectedResult, result1, "finder1")
-    verifyResult(n, expectedResult, result2, "finder2")
+    val result1 = pf.findParenCombinations(n)
+    //val result2 = new ParenFinder2().findParenCombinations(n)
+    verifyResult(n, expectedResult, result1, "finder1")
+    //verifyResult(n, expectedResult, result2, "finder2")
   }
 
   def verifyResult(n: Int, expectedResult: Seq[String], actResult: Seq[String], msg: String): Unit = {
@@ -128,7 +139,7 @@ class SolutionTest extends FunSuite {
   }
 
   def verifyNum(n: Int, expectedNumCombos: Int): Unit = {
-    val result2 = new ParenFinder2().findParenCombinations(n)
+    val result2 = pf.findParenCombinations(n)
     assertResult(expectedNumCombos) { result2.length }
   }
 }
